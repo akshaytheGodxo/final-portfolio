2784c5c477171bbeae88139f0a8810ee73f4ea01
akshaytheGodxo <akshaysinghc230@gmail.com>
2025-10-05 20:25:21 +0530

    Add MainHome component with layout and links for personal portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+
+const jetBrainsMono = JetBrains_Mono({
+  variable: "--font-jetbrains-mono",
+  subsets: ["latin"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${jetBrainsMono.variable} antialiased`}
       >
         {children}
       </body>
---
+++ src/components/Home.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function MainHome() {
+    return (
+        <section className="min-h-screen grid grid-cols-1 md:grid-cols-3  ">
+            <div className="flex flex-col ">
+                <Image
+                    src="/laptop-typer.png"
+                    alt="Typing in Laptop"
+                    width={250}
+                    height={221}
+                    className="mt-22 ml-5"
+                />
+            </div>
+
+            <div className="flex flex-col items-center  text-center space-y-12 mt-0">
+                <h2 className="font-[Jetbrains_Mono] mt-4 text-black text-4xl font-thin">
+                    Akshay Singh Chauhan
+                </h2>
+
+                <div className="grid grid-rows-3 gap-8">
+                    <Link
+                        href="#home"
+                        className="font-[Jetbrains_Mono] text-black text-8xl"
+                    >
+                        home
+                    </Link>
+                    <Link
+                        href="#experience"
+                        className="font-[Jetbrains_Mono] text-black text-8xl"
+                    >
+                        experience
+                    </Link>
+                    <Link
+                        href="#about"
+                        className="font-[Jetbrains_Mono] text-black text-8xl"
+                    >
+                        about
+                    </Link>
+                </div>
+
+                <div className="grid grid-rows-2 gap-6.5 font-[Jetbrains_Mono]">
+                    <button className="w-[304px] h-[68px] bg-black text-white rounded-[45px]">
+                        Let's connect
+                    </button>
+                    <button className="w-[304px] h-[68px] bg-transparent border border-black text-black rounded-[45px]">
+                        View resume
+                    </button>
+                </div>
+                <div className="grid grid-cols-2 ">
+                    <Link
+                        href={"https://github.com/akshaytheGodxo/"}
+                        target="_blank"
+                    >
+                        <Image
+                            src={"/GitHub.png"}
+                            alt="GitHub"
+                            width={65}
+                            height={59}
+                        />
+                    </Link>
+                    <Link
+                        href={"https://www.linkedin.com/in/akshay-singh-chauhan-wow/"}
+                        target="_blank"
+                    >
+                        <Image
+                            src={"/LinkedIn Circled.png"}
+                            alt="LinkedIn"
+                            width={72}
+                            height={59}
+                        />
+                    </Link>
+                </div>
+            </div>
+
+            <div className="hidden md:block"></div>
+        </section>
+    )
+}
